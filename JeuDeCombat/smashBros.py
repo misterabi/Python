e12b26d73c56a98926f74fd80a0fb36fa7b36d9c
Sinna <sinna1704@gmail.com>
2019-03-23 21:43:26 +0100

    Update smashBros.py

--- JeuDeCombat/smashBros.py
+++ JeuDeCombat/smashBros.py
@@ -1,30 +1,5 @@
 import pygame
 
-"""
-Jeu Smash Bros
-Objectif : Survivre
-Nombre de joueurs maximum : 2
-Nombre de vie par joueur  : 3
-Règles de jeu :
- Joueur 1 :
-	Touches directionnelle : déplacer
-	Touche ESPACE : sauter
-	Touche V : attaquer 
- Joueur 2 : 
-	Touches Z,Q,S,D : déplacer
-	Touche 0 : sauter
-	Touche 1 : attaquer 
- Si un joueur A attaque un joueur B alors le joueur B perd une vie.
-
- Définition de attaquer : premier qui touche l'autre
-
-Phase de développement:
-1/ Afficher 2 joueurs sur un écran
-2/ Déplacer un joueur sur l'écran en fonction des clics clavier
-3/ Attaquer un joueur
-4/ Calculer victoire
-"""
-
 bleu = (14, 180, 245)
 black = (0, 0, 0)
 
