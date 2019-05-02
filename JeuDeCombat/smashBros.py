e10c2a43740a66f2fe7f056b07278e10545f4f2b
misterabi <48652667+misterabi@users.noreply.github.com>
2019-05-02 16:13:44 +0200

    Update smashBros.py

--- JeuDeCombat/smashBros.py
+++ JeuDeCombat/smashBros.py
@@ -57,12 +57,16 @@ class player(pygame.sprite.Sprite):
         self.image = self.spriteSheet.subsurface(pygame.Rect(0, 0, 64, 90))
         self.rect = pygame.Rect(0, 0, 32, 64)
         self.rect.center = position
-        self.isJump=False
-        self.jumpCount=10
+        self.healt=100
+        self.alive=True
+
         self.sens_personnage_droite=True
         self.sens_personnage_gauche=False
 
-        self.cooldown=1
+        self.isJump=False
+        self.jumpCount=10
+        self.cooldownSuperPower=15
+        self.cooldownFire=1
         self.numeroSequence = 0
         self.numeroImage = 0
         self.flip = False
@@ -118,29 +122,39 @@ class player(pygame.sprite.Sprite):
 
     def shoot(self,speed):
         if self.sens_personnage_droite is True:
-            ball = Ball((self.rect.centerx+30,self.rect.centery),speed)
+            ball = Ball((self.rect.centerx+30,self.rect.centery),speed,"image/fireball1.png",False)
         if self.sens_personnage_gauche is True:
-            ball = Ball((self.rect.centerx-30,self.rect.centery),speed)
+            ball = Ball((self.rect.centerx-30,self.rect.centery),speed,"image/fireball1.png",True)
 
         bullets.add(ball)
 
+    def SuperPower(self,speed):
+        if self.sens_personnage_droite is True:
+            Superball = Ball((self.rect.centerx+30,self.rect.centery),speed,"image/Superfireball.png",False)
+        if self.sens_personnage_gauche is True:
+            Superball = Ball((self.rect.centerx-30,self.rect.centery),speed,"image/Superfireball.png",True)
+        SuperPower.add(Superball)
+
 class Ball(pygame.sprite.Sprite):
-    def __init__(self,position,speed):
+    def __init__(self,position,speed,image,flip):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("image/fireball1.png").convert_alpha()
+        self.image = pygame.image.load(image).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.center = position
         self.speed= speed
+        self.flip= flip
     def update(self):
         self.rect.x += self.speed
+        if self.flip:
+            self.image = pygame.transform.flip(self.image, True, False)
         if self.rect.y > 720:
             self.kill()
-def mouvement(joueur,ennemie,leurre,gauche,droite,accroupir,saut,tire,coup):
+def mouvement(joueur,ennemie,leurre,gauche,droite,accroupir,saut,tire,coup,bloc,hitFire,hitSuperPower,):
     global directionVitesse
     if event.type == pygame.KEYDOWN and event.key is leurre or droite or gauche or accroupir or saut or  tire or coup :
         if gauche and joueur.rect.x > 1:
             joueur.goLeft()
-        if droite and ennemie.rect.x < 685 and not joueur.rect.colliderect(ennemie.rect):
+        if droite and joueur.rect.x < 685 :
             joueur.goRight()
 
         if event.type == pygame.KEYUP:
@@ -152,16 +166,7 @@ def mouvement(joueur,ennemie,leurre,gauche,droite,accroupir,saut,tire,coup):
                 joueur.sens_personnage_droite=False
                 joueur.sens_personnage_gauche=True
                 directionVitesse=-5
-        if joueur.cooldown >=1 and tire :
-            pygame.time.Clock().tick(50)
-            joueur.punch()
-            abi.cooldown -= 1
-            if joueur.rect.colliderect(ennemie.rect):
-                print("toucher")
 
-        if joueur.cooldown >=1.5 and coup:
-            joueur.shoot(directionVitesse)
-            joueur.cooldown -= 2
         if not joueur.isJump:
             if accroupir:
                 joueur.crouch()
@@ -182,15 +187,61 @@ def mouvement(joueur,ennemie,leurre,gauche,droite,accroupir,saut,tire,coup):
             else:
                 joueur.isJump = False
                 joueur.jumpCount = 10
+#coup de poing#
+        if joueur.cooldownFire >= 1 and coup :
+            joueur.punch()
+            joueur.cooldownFire -= 1
+            if joueur.rect.colliderect(ennemie.rect):
+                if ennemie.rect.x > 10 and ennemie.rect.x < 670:
+                    ennemie.healt-=2.5
+                    ennemie.rect = ennemie.rect.move(directionVitesse * 3, 0)
+                    print("toucher")
+                    joueur.cooldownSuperPower -= 1
+
+#boule de feu#
+        if joueur.cooldownFire >= 1.5 and tire:
+            joueur.shoot(directionVitesse)
+            joueur.cooldownFire -= 2
+# Super Attaque#
+        if tire and coup  and joueur.cooldownSuperPower <=0:
+            joueur.SuperPower(directionVitesse)
+            joueur.cooldownSuperPower=15
+        if tire and coup  and joueur.cooldownSuperPower >=0:
+           print("il vous reste:",joueur.cooldownSuperPower)
+
 
     else:
         joueur.stand()
-    if joueur.cooldown <1.6:
-        joueur.cooldown+=0.1
+    if joueur.cooldownFire <1.6:
+        joueur.cooldownFire+=0.1
+#collision attaque distance#
+    if bloc and hitFire:
+        joueur.healt-=0
+    elif not bloc and hitFire:
+        joueur.healt -= 2
+        ennemie.cooldownSuperPower -= 0.5
+        if joueur.rect.x >10 and joueur.rect.x < 670:
+            joueur.rect=joueur.rect.move(directionVitesse*3,0)
+
+    if hitSuperPower:
+        joueur.healt -= 15
+    if joueur.healt <=0:
+        joueur.alive=False
+        joueur.kill()
+
+
+    print(joueur.healt)
+def EndGame(playeurHealth1,playeurHealth2):
+    global  launched
+    if not playeurHealth1 :
+        pygame.display.quit
+    if not playeurHealth2:
+        pygame.display.quit
+
 
 # creation des joueur
 all_sprite=pygame.sprite.Group()
-damage=pygame.sprite.Group()
+SuperPower=pygame.sprite.Group()
 bullets=pygame.sprite.Group()
 enemie=pygame.sprite.Group()
 allie=pygame.sprite.Group()
@@ -230,32 +281,28 @@ while launched:
     bloc2=key[pygame.K_RSHIFT]
 
     test=key[pygame.K_j]
-    hits1 = pygame.sprite.groupcollide(bullets, enemie, True, False)
-    hits2 = pygame.sprite.groupcollide(bullets, allie, True, False)
+    hitsFirePlayer1 = pygame.sprite.groupcollide(bullets,allie , True, False)
+    hitsFirePlayer2 = pygame.sprite.groupcollide(bullets, enemie, True, False)
+    hitSuperPowerPlayer1=pygame.sprite.groupcollide(SuperPower,allie , True, False)
+    hitSuperPowerPlayer2 = pygame.sprite.groupcollide(SuperPower, enemie, True, False)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             launched = False
-    mouvement(abi,JC,test,Left1,Right1,Down1,Jump1,fire1,punching1)
-    mouvement(JC,abi,test,Left2,Right2,Down2,Jump2,fire2,punching2)
+    mouvement(abi,JC,test,Left1,Right1,Down1,Jump1,fire1,punching1,bloc1,hitsFirePlayer1,hitSuperPowerPlayer1)
+    mouvement(JC,abi,test,Left2,Right2,Down2,Jump2,fire2,punching2,bloc2,hitsFirePlayer2,hitSuperPowerPlayer2)
 
-    if bloc2 and hits1:
-        print("no damage,player2")
-    elif not bloc1 and hits1:
-        print("aie,player2")
-    if bloc1 and hits2:
-        print("no damage,player1")
-    elif  not bloc2 and hits2:
-        print("aie,player1")
+    if not abi.alive:
+        launched=False
+    if not JC.alive:
+        launched=False
 
-
-
-
-
+    SuperPower.update()
     bullets.update()
     all_sprite.update()
     fenetre_smash_bros.fill(bleu)
     pygame.draw.rect(fenetre_smash_bros, black, rectangle)
     all_sprite.draw(fenetre_smash_bros)
     bullets.draw(fenetre_smash_bros)
+    SuperPower.draw(fenetre_smash_bros)
 
     pygame.display.flip()
